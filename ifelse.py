74d6b3156a59c157917d7156223b714cb9646bf6
Bharatkumarsah <56533019+Bharatkumarsah@users.noreply.github.com>
2020-01-05 21:10:31 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number ="))
+if(a>=20):
+    print("greater")
+    if(a%2==0):
+        print("even number")
+    else:
+        print("odd number")
+else:
+       print("lesser")
+       print("complete")
